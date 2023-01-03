e8f32eeb3e0e2c1d815917b7aa770f4241b6fe91
ian29012 <iancheng29012@gmail.com>
2023-01-03 15:13:22 +0000

    updated the forecast section

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,11 @@ function render(searchCity){
     if ($("#today").children) {
         $("#today").empty();
     }
-    
+
+    if ($("#forecast").children) {
+        $("#forecast").empty();
+    }
+
     var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + searchCity + "&units=metric&limit=5&appid=" + API_KEY
     console.log(queryURL) 
 
@@ -14,12 +18,28 @@ $.ajax({
     method: "GET"
 }).then(function(response){
 
-        $("#today").append($('<p class="current" id="thedate">' + response.city.name + ' ( ' + moment.unix(response.list[0].dt).format("D MMM YYYY") + ' )</p>'))
+        $("#today").append($('<p class="current" id="thedate">' + response.city.name + ' ( ' + moment.unix(response.list[0].dt).format("MM-DD-YYYY") + ' )</p>'))
         var weathericon = "http://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + "@2x.png"
         $("#today").append($('<img class="current" id="icon">').attr("src", weathericon))
-        $("#today").append($('<p class="current" id="Temperature">' + response.list[0].main.temp + "°C" + '</p>'))
+        $("#today").append($('<p class="current" id="Temperature">' + response.list[0].main.temp.toFixed(1) + "°C" + '</p>'))
         $("#today").append($('<p class="current" id="humidity">' + 'Humidity : ' + response.list[0].main.humidity + " %" + '</p>'))
+        var windspeed = response.list[0].wind.speed * 2.236936
+        $("#today").append($('<p class="current" id="windspeed">' + 'Wind Speed : ' + windspeed.toFixed(1) + " mph" + '</p>'))
         console.log(response);
+
+        for (var i = 8; i < 41; i+=7 ){
+
+        var forecast = $("<div class='col-lg-1.8 forecast'>")
+        forecast.append($('<p class="forecast" id="thedate">' + moment.unix(response.list[i].dt).format("MM-DD-YYYY") + '</p>'))
+        var weathericon = "http://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + "@2x.png"
+        forecast.append($('<img class="forecast" id="icon">').attr("src", weathericon))
+        forecast.append($('<p class="forecast" id="Temperature">' + response.list[i].main.temp + "°C" + '</p>'))
+        forecast.append($('<p class="forecast" id="humidity">' + 'Humidity : ' + response.list[i].main.humidity + " %" + '</p>'))
+
+        $("#forecast").append(forecast)
+console.log(i)
+        }
+//     }
         $("#history").prepend($("<button class='historybutton' id='historybutton'>" + searchCity + "</button>"))
         $('[name="inputvalue"]').val("");
     })
